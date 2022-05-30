ef6dfc2e38cbf339908503db39813a1997412228
L.H <michael123303@yahoo.com>
2022-05-30 20:27:35 +0800

    修正語法符合PEP8
    修正語法符合PEP8

--- firefox/login_firefox.py
+++ firefox/login_firefox.py
@@ -1,28 +1,31 @@
 import selenium
 from selenium import webdriver
-import configparser, sys
+import configparser
+import sys
 
 config = configparser.ConfigParser()
 config.read("..\\config.ini")
 
-url='https:\\\\sts.knu.edu.tw\\'
+url = 'https:\\\\sts.knu.edu.tw\\'
 
 sid = config['login_web']['username']
 upwd = config['login_web']['password']
+
 
 def login():
     driver = webdriver.Firefox()
     driver.implicitly_wait(3)
     driver.get(url)
-    
+
     username = driver.find_element_by_id('Editbox1')
     username.send_keys(sid)
-    
+
     pwd = driver.find_element_by_id('Editbox2')
     pwd.send_keys(upwd)
-    
+
     login = driver.find_element_by_id('buttonLogOn')
     login.click()
+
 
 if __name__ == '__main__':
     login()
--- ie/login_ie.py
+++ ie/login_ie.py
@@ -4,33 +4,37 @@ DO NOT EDIT UNLESS YOU ARE SURE THAT YOU KNOW WHAT YOU ARE DOING
 '''
 import selenium
 from selenium import webdriver
-import configparser, sys
+import configparser
+import sys
 
 config = configparser.ConfigParser()
 config.read("..\\config.ini")
 
-url='http:\\\\portal.knu.edu.tw\\knue2\\'
+url = 'http:\\\\portal.knu.edu.tw\\knue2\\'
 
 Type = config['login_web']['type']
 sid = config['login_web']['username']
 upwd = config['login_web']['password']
+
 
 def login():
     driver = webdriver.Ie()
     driver.implicitly_wait(3)
     driver.get(url)
 
-    login_type = driver.find_element_by_xpath("//input[@name='USER_STATUS' and @value='%s']" %(Type)) #value='2' student / value='1' teacher
+    login_type = driver.find_element_by_xpath(
+        "//input[@name='USER_STATUS' and @value='%s']" % (Type))  # value='2' student / value='1' teacher
     login_type.click()
-    
+
     username = driver.find_element_by_name('EMPNO')
     username.send_keys(sid)
-    
+
     pwd = driver.find_element_by_name('loginPassword')
     pwd.send_keys(upwd)
-    
+
     login = driver.find_element_by_id('button1')
     login.click()
+
 
 if __name__ == '__main__':
     login()
